34df5493fc583205d9cff5dc7d4a907e2b8237cc
lifests <166196017+lifests@users.noreply.github.com>
2024-04-09 12:29:02 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -20,7 +20,7 @@ export default siteConfig({
   twitter: 'null',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  newsletter: 'com',
+  email: 'junjie@lifests.com',
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
